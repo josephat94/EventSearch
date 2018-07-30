4eac79f7501baa1f259a07bfee94a1d4b4f7a98f
josephat <josephat.reyes@HP-LaserJet.local>
2018-07-30 10:27:40 -0500

    En esta version se aqrego el metodo para obtener los espacios paginados

--- application/controllers/Espacio.php
+++ application/controllers/Espacio.php
@@ -18,5 +18,45 @@ header("Access-Control-Allow-Origin: *");
 
 
 
+
+ 
+    
+    
+//Este metodo retorna una pagina de espacios en conteo de 10 en 10 recibe el numero de la pagina que necesitas
+  public function getEspacios_get($index=0){
+    
+$pagina= $index*10;
+    $diez=10;
+
+
+    $query= $this->db->query('SELECT * FROM `Espacio` limit '.$pagina.','.$diez);
+
+
+    $Espacios= $query->result_array();
+$ArregloCompleto= array();
+
+    foreach ($Espacios as $espacio) {
+
+     $llave=$espacio['pk_espacio'];
+        $query= $this->db->query('SELECT * FROM `imagen` WHERE fk_espacio = '.$llave);
+        $Imagenes= $query->result_array();
+        $obj= new stdClass();
+        $obj->Espacio= $espacio;
+        $obj->Imagenes= $Imagenes;
+
+array_push($ArregloCompleto,$obj);
+
+    }
+
+$respuesta=array("Error"=>"FALSE", "Espacios"=>$ArregloCompleto);
+
+
+$this->response($respuesta);
+
+  }  
+
+
+
+
     
 }
